b4d4350b725a252986449054bcad347de8df098f
kooky-optimization-7340 <christinafox93@gmail.com>
2019-05-03 23:33:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.chomp
+  user_input = gets.strip
   input_to_index(user_input)
   if valid_move(board,index) == true
     move(board,index)
