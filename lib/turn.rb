0c8a9b7c1039d07a4b6b4e240c223909d5f1acc7
kooky-optimization-7340 <christinafox93@gmail.com>
2019-05-03 22:56:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,25 @@ def display_board
   puts "-----------"
   puts "   |   |   "
 end
+
+def valid_move?(board,index)
+!position_taken?(board,index) && index.between?(0,8)
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    taken = false
+  else
+    taken = true
+  end
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+end
+
