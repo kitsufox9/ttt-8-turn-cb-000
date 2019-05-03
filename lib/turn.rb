ebc2d6e6d7797c8fd77f29728908eb43247afc85
kooky-optimization-7340 <christinafox93@gmail.com>
2019-05-03 23:51:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,5 +36,9 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index) == true
-
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
