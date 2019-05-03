81e37ac75c651950b66f65131c8292a4578e6d91
kooky-optimization-7340 <christinafox93@gmail.com>
2019-05-03 22:58:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,7 +10,6 @@ def valid_move?(board,index)
 !position_taken?(board,index) && index.between?(0,8)
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     taken = false
