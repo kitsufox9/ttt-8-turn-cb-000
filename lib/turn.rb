e0930796c3bffd133e913675b8557bcfa5518f8d
kooky-optimization-7340 <christinafox93@gmail.com>
2019-05-03 23:11:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,34 +1,32 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def valid_move?(board,index)
-!position_taken?(board,index) && index.between?(0,8)
-end
-
-def position_taken?(board,index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    taken = false
-  else
-    taken = true
-  end
-end
-
-def input_to_index(user_input)
-  index = user_input.to_i - 1
-end
-
-def move(board, index, character = "X")
-  board[index] = character
-end
-
-def turn
-end
-
 def turn(board)
   puts "Please enter 1-9:"
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  def valid_move?(board,index)
+  !position_taken?(board,index) && index.between?(0,8)
+  end
+
+  def position_taken?(board,index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      taken = false
+    else
+      taken = true
+    end
+  end
+
+  def input_to_index(user_input)
+    index = user_input.to_i - 1
+  end
+
+  def move(board, index, character = "X")
+    board[index] = character
+  end
+
+  def turn
+  end
 end
