6467000eee2672e2fcf255f91e273a9a288410e9
kooky-optimization-7340 <christinafox93@gmail.com>
2019-05-03 23:13:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,29 +6,29 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def valid_move?(board,index)
+!position_taken?(board,index) && index.between?(0,8)
+end
+
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    taken = false
+  else
+    taken = true
+  end
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+end
+
+def turn
+end
+
 def turn(board)
   puts "Please enter 1-9:"
-
-  def valid_move?(board,index)
-  !position_taken?(board,index) && index.between?(0,8)
-  end
-
-  def position_taken?(board,index)
-    if board[index] == " " || board[index] == "" || board[index] == nil
-      taken = false
-    else
-      taken = true
-    end
-  end
-
-  def input_to_index(user_input)
-    index = user_input.to_i - 1
-  end
-
-  def move(board, index, character = "X")
-    board[index] = character
-  end
-
-  def turn
-  end
 end
